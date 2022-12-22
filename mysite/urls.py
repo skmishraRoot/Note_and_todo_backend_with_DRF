4183f33103158cec43f6909894dc46f9468474d2
Sumit <115337317+skmishraRoot@users.noreply.github.com>
2022-12-22 21:38:33 +0530

    adding urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import Home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Home, name='Home-page'),
     path('', include('todoapi.urls')),
     path('', include('Notesapi.urls')),
 ]

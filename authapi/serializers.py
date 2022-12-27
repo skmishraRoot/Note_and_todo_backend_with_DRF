ec23f0052f592159b80554d555bda922778f9c87
Sumit <115337317+skmishraRoot@users.noreply.github.com>
2022-12-27 13:36:00 +0530

    changing in user register

--- authapi/serializers.py
+++ authapi/serializers.py
@@ -10,27 +10,16 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 # Making serializer for Registering a User
 class RegisterUserSerializer(serializers.ModelSerializer):
-    # Defining some extra things for these fields
-    email = serializers.EmailField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
-    password = serializers.CharField(write_only=True, required=True,validators=[validate_password])
-
     # Defining fields and model
     class Meta:
         model = User
-        fields = ['username','password','email', 'first_name', 'last_name']
-        extra_kwargs = {
-            'first_name': {'required': True},
-            'last_name': {'required': True}
-        }
+        fields = ['username','password','email']
     # Creating user in the database
     def create(self, validate_data):
         user = User.objects.create(
             username=validate_data['username'],
             email=validate_data['email'],
-            first_name=validate_data['first_name'],
-            last_name=validate_data['last_name'],
         )
-
         user.set_password(validate_data['password'])
         user.save()
         return user

0b308b83e64520cbbefe4eaee136d4d008a7b44f
Sumit <115337317+skmishraRoot@users.noreply.github.com>
2022-12-29 12:56:57 +0530

    makin changes to route

--- mysite/views.py
+++ mysite/views.py
@@ -5,8 +5,8 @@ from rest_framework.decorators import api_view
 @api_view(['GET'])
 def Home(request):
     routes = {
-        'Todo_app':"/api/tasks",
-        'Notes_app':"/api/Notes",
-        'accounts':"/register"
+        'Todo_app':"/api/todo = Todo application api",
+        'Notes_app':"/api/Notes = Note application api",
+        'accounts':"/register = auth register api"
     }
     return  Response(routes)
